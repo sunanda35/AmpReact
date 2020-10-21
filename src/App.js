8a38184466f94180648bbcd0af78766bde725038
sunanda <sunandasamanta38@gmail.com>
2020-10-21 06:09:29 +0530

    Test for using react-AMP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as Amp from 'react-amphtml';
+import * as AmpHelpers from 'react-amphtml/helpers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <AmpHelpers.Action >
+  {(props) => (
+    <button type="button" {...props}>
+      Do Something
+    </button>
+  )}
+</AmpHelpers.Action>
+      </div>
+    );
 }
 
 export default App;
