3158aace1750aa56521ef08a431772d8a02ab7f6
sunanda <sunandasamanta38@gmail.com>
2020-10-26 00:53:39 +0530

    amp test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import * as Amp from 'react-amphtml';
-import * as AmpHelpers from 'react-amphtml/helpers';
 
 function App() {
   return (
       <div>
-        <AmpHelpers.Action >
-  {(props) => (
-    <button type="button" {...props}>
-      Do Something
-    </button>
-  )}
-</AmpHelpers.Action>
-      </div>
+        hi how are you
+        </div>
     );
 }
 
